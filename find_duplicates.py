8e3c64a5090b7bc209d5300ec3cb0f10f2f07423
Jacotsu <Jacotsu@users.noreply.github.com>
2018-06-23 23:52:14 +0200

    Implemented multiprocessing Added sqlite database

--- find_duplicates.py
+++ find_duplicates.py
@@ -9,13 +9,12 @@ from pathlib import PurePath
 import os
 import sys
 import logging
-import json
 import yaml
 import argparse
 import itertools
-from concurrent.futures import ThreadPoolExecutor
-from concurrent.futures import wait
-from concurrent.futures import ALL_COMPLETED
+import threading
+import ast
+from multiprocessing import Pool
 
 max_threads = 8
 
@@ -25,7 +24,7 @@ class DbMan:
 
     def __init__(self,
                  db='image_recognizer.db',
-                 queries_file='queries.sql'):
+                 queries_file='queries.yaml'):
         if not DbMan.queries:
             with open(queries_file, 'r') as config:
                 try:
@@ -33,22 +32,118 @@ class DbMan:
                 except yaml.YAMLError as exc:
                     logging.error(exc)
                     raise exc
+        self.db = db
+        self.conn = {threading.current_thread(): sqlite3.connect(db)}
+        with self.conn[threading.current_thread()] as conn:
+            conn.execute(DbMan.queries['create_metadata_table'])
+            conn.execute(DbMan.queries['create_paths_table'])
 
-        self.conn = sqlite3.connect(db)
-        with self.conn:
-            self.conn.execute(DbMan.queries['create_tables'])
+    def insert_images_data(self, images):
+        if not self.conn[threading.current_thread()]:
+            self.conn[threading.current_thread()] = sqlite3.connect(self.db)
 
-    def insert_images_data(self, image_tuples):
-        if image_tuples is not list:
-            image_tuples = [image_tuples]
+        if type(images) is not list:
+            images = [images]
 
-        with self.conn:
-            self.con.executemany(DbMan.queries['insert_image_metadata'],
-                                 image_tuples)
+        with self.conn[threading.current_thread()] as conn:
+            signatures = [(image['hash'],
+                           str(image['signature'])) for image in images]
+            conn.executemany(DbMan.queries['insert_image_metadata'],
+                             signatures)
+            for image in images:
+                paths = [(image['hash'], path) for path in
+                         image['path']]
+                conn.executemany(DbMan.queries['insert_path'],
+                                 paths)
+            logging.debug('Inserted {} in database'.format(signatures))
 
     def get_images_data(self, image_hashes):
-        if image_hashes is not list:
+        if not self.conn[threading.current_thread()]:
+            self.conn[threading.current_thread()] = sqlite3.connect(self.db)
+
+        if type(image_hashes) is not list:
             image_hashes = [image_hashes]
+
+        with self.conn[threading.current_thread()] as conn:
+            results = []
+            for img_hash in image_hashes:
+                res = conn.execute(DbMan.queries['get_image_metadata'],
+                                   (img_hash,))
+                data = res.fetchone()
+                if data:
+                    res = conn.execute(DbMan.queries['get_paths'],
+                                       (img_hash,))
+
+                    paths = res.fetchall()
+                    if paths:
+                        paths = [ast.literal_eval(path[0]) for path in paths]
+                    else:
+                        paths = []
+
+                    results.append({'hash': data[0],
+                                    'signature': ast.literal_eval(data[1]),
+                                    'path': paths})
+                    logging.debug('Extracted {} from database'.format(data))
+
+    def get_all_images(self, page=0):
+        if not self.conn[threading.current_thread()]:
+            self.conn[threading.current_thread()] = sqlite3.connect(self.db)
+
+        with self.conn[threading.current_thread()] as conn:
+            results = []
+            res = conn.execute(DbMan.queries['get_all_images'])
+            if res:
+                for img in res.fetchall():
+                    img_paths = conn.execute(DbMan.queries['get_paths'],
+                                             (img[0],))
+                    results.append({'hash': img[0],
+                                    'signature': ast.literal_eval(img[1]),
+                                    'path': [path[0] for path in
+                                             img_paths.fetchall()]})
+            logging.info('Extracted all images from database')
+            return results
+
+    def check_images_presence(self, images):
+        if not self.conn[threading.current_thread()]:
+            self.conn[threading.current_thread()] = sqlite3.connect(self.db)
+
+        if type(images) is not list:
+            images = [images]
+
+        non_present_images = []
+        with self.conn[threading.current_thread()] as conn:
+            for img in images:
+                res = conn.execute(DbMan.queries['check_hash_existence'],
+                                   (img['hash'],))
+
+                if not res.fetchone():
+                    non_present_images.append(img)
+
+            return non_present_images
+
+    def add_imgs_paths(self, img):
+        if not self.conn[threading.current_thread()]:
+            self.conn[threading.current_thread()] = sqlite3.connect(self.db)
+
+        if type(img) is not list:
+            img = [img]
+
+        path_list = []
+        for simg in img:
+            path_list += [(simg['hash'], path) for path in simg['path']]
+
+        with self.conn[threading.current_thread()] as conn:
+            conn.executemany(DbMan.queries['insert_path'],
+                             path_list)
+
+    def get_img_path(self, img):
+        if not self.conn[threading.current_thread()]:
+            self.conn[threading.current_thread()] = sqlite3.connect(self.db)
+
+        with self.conn[threading.current_thread()] as conn:
+            results = conn.execute(DbMan.queries['get_path'],
+                                   (img['hash'],))
+            return [res[0] for res in results]
 
 
 def md5sum(filename):
@@ -59,27 +154,36 @@ def md5sum(filename):
     return d.hexdigest()
 
 
-def calculate_file_signature(file_full_path, gis, db):
+def calculate_file_signature(file_full_path, gis, db=DbMan()):
+    file_full_path = str(file_full_path)
     file_hash = str(md5sum(file_full_path))
 
-    if file_hash in db.keys():
-        logging.debug('Signature of {} already in database'
-                      .format(file_full_path))
-        return
-    try:
-        image_signature = gis.generate_signature(file_full_path)
-        db[file_hash] = {'signature': image_signature.tolist(),
-                         'path': file_full_path
-                         }
-        logging.debug('Calculated signature of {}: \n{}'
-                      .format(file_full_path, image_signature))
-    except:
-        logging.error('An error occurred while processing {}'
-                      .format(file_full_path))
+    if not db.check_images_presence({'hash': file_hash}):
+        logging.info('Signature of {} already in database'
+                     .format(file_full_path))
+        try:
+            db.add_imgs_paths({'hash': file_hash, 'path': [file_full_path]})
+            logging.info('Added path {}'.format(file_full_path))
+        except sqlite3.IntegrityError:
+            pass
+    else:
+        try:
+            image_signature = gis.generate_signature(file_full_path)
+            img = {'signature': image_signature.tolist(),
+                   'hash': file_hash,
+                   'path': [file_full_path]
+                   }
+            db.insert_images_data(img)
+            logging.debug('Calculated signature of {}: \n{}'
+                          .format(file_full_path, image_signature))
+        except Exception as ex:
+            logging.error('An error occurred while processing {}'
+                          .format(file_full_path))
+            logging.error(ex)
 
 
 def calculate_batch_signature(files_full_path):
-    if files_full_path is not list:
+    if type(files_full_path) is not list:
         files_full_path = [files_full_path]
 
     gis = ImageSignature()
@@ -103,74 +207,46 @@ def calculate_batch_signature(files_full_path):
     return signatures
 
 
-def calculate_signatures(root_path, database='signatures.json'):
-    executor = ThreadPoolExecutor(max_workers=max_threads)
-    fs = []
-
+def calculate_signatures(root_path, db=DbMan()):
     gis = ImageSignature()
-    db = {}
 
-    try:
-        with open(database, 'r') as data:
-            db = json.load(data)
-    except FileNotFoundError:
-        pass
+    with Pool(max_threads) as ppool:
+        full_paths = []
+        for path, subdirs, files in os.walk(root_path):
+            for name in files:
+                full_paths.append((PurePath(path, name), gis))
 
-    for path, subdirs, files in os.walk(root_path):
-        for i, name in enumerate(files):
-            file_full_path = str(PurePath(path, name))
-
-            fs.append(executor.submit(calculate_file_signature,
-                                      file_full_path,
-                                      gis,
-                                      db)
-                      )
-            if (i % max_threads == 0):
-                wait(fs, timeout=None, return_when=ALL_COMPLETED)
-                fs = []
-
-    with open(database, 'w') as data:
-        json.dump(db, data)
+        ppool.starmap(calculate_file_signature, full_paths)
 
 
-def match_images(img1, img2, threshold, db, gis):
-    dis = gis.normalized_distance(np.array(db[img1]['signature']),
-                                  np.array(db[img2]['signature']))
+def match_images(img1, img2, threshold, gis, db=DbMan()):
+    dis = gis.normalized_distance(np.array(img1['signature']),
+                                  np.array(img2['signature']))
+
+    if len(img1['path']) > 1:
+        logging.info('Similar images found: {:.5f} \n {} \n'
+                     .format(dis, '\n'.join(img1['path'])))
+
+    if len(img2['path']) > 1:
+        logging.info('Similar images found: {:.5f} \n {} \n'
+                     .format(dis, '\n'.join(img2['path'])))
+
     if dis < threshold:
         logging.info('Similar images found: {:.5f} \n {} \n {} \n'
                      .format(dis,
-                             db[img1]['path'],
-                             db[img2]['path']))
+                             img1['path'],
+                             img2['path']))
 
 
-def find_matches(threshold, database='signatures.json'):
-    executor = ThreadPoolExecutor(max_workers=max_threads)
-    fs = []
-
+def find_matches(threshold, db=DbMan()):
     gis = ImageSignature()
-    db = {}
+    data = []
 
-    try:
-        with open(database, 'r') as data:
-            db = json.load(data)
-    except FileNotFoundError:
-        logging.error("Database not found, run find_duplicates update first!")
-        return
+    for img1, img2 in itertools.combinations(db.get_all_images(), 2):
+        data.append((img1, img2, threshold, gis))
 
-    i = 0
-    for img1, img2 in itertools.combinations(db.keys(), 2):
-        i += 1
-        fs.append(executor.submit(match_images,
-                                  img1,
-                                  img2,
-                                  threshold,
-                                  db,
-                                  gis
-                                  )
-                  )
-        if (i % max_threads == 0):
-            wait(fs, timeout=None, return_when=ALL_COMPLETED)
-            fs = []
+    with Pool(max_threads) as ppool:
+        ppool.starmap(match_images, data)
 
     logging.info('Image matching finished')
 
