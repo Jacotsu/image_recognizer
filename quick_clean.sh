8f8f6e4f795758438c87d81c1ad0c0a7fff288be
Jacotsu <Jacotsu@users.noreply.github.com>
2018-06-24 09:31:31 +0200

    Now cleaning script shouldn't generate command strings too long

--- quick_clean.sh
+++ quick_clean.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 files=()
-
+counter=0
 
 MATCHES_REGEX='/Image/,/Image/p'
 PATHS_REGEX='(^\/.*$)'
@@ -11,5 +11,12 @@ PATHS=$(grep -Po "$PATHS_REGEX" <<<$MATCHES)
 
 for entry in $PATHS; do
     files+=("$entry")
+    ((counter++))
+
+
+    if (( $counter > 10 )); then
+        eog ${files[*]}
+        files=()
+    fi
 done
 eog ${files[*]}
