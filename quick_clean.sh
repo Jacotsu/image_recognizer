f3517b94d515ee518bfe1b26d4dc47dca84af1a4
Jacotsu <Jacotsu@users.noreply.github.com>
2018-06-24 10:25:23 +0200

    Fixed minor bug in quick_clean script

--- quick_clean.sh
+++ quick_clean.sh
@@ -10,13 +10,15 @@ MATCHES=$(sed -n "$MATCHES_REGEX" $1)
 PATHS=$(grep -Po "$PATHS_REGEX" <<<$MATCHES)
 
 for entry in $PATHS; do
-    files+=("$entry")
-    ((counter++))
-
+    if [ -f $entry ]; then
+        files+=("$entry")
+        ((counter++))
+    fi
 
     if (( $counter > 10 )); then
         eog ${files[*]}
         files=()
+        counter=0
     fi
 done
 eog ${files[*]}
